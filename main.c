2c0d504db22b22c86b6eb9807eafd0a37bfc8b16
janmayos <jangasca5@gmail.com>
2022-06-08 22:40:38 -0500

    validación

--- main.c
+++ main.c
@@ -13,8 +13,7 @@
 int main(int argc, char const *argv[])
 {
 
-    if (argc < 6)
-    {
+    if (argc < 6){
         menu();
         return 0;
     }
@@ -24,26 +23,22 @@ int main(int argc, char const *argv[])
     mode = atoi(argv[3]);
     view = atoi(argv[4]);
     times = atoi(argv[5]);
-    if (rows <= 0 && columns <= 0)
-    {
+    if (rows <= 0 && columns <= 0){
         printf("Dimensiones del tablero no permitidas");
         return 0;
     }
 
-    if (mode < 1 || mode > 2)
-    {
+    if (mode < 1 || mode > 2){
         printf("Modo de operación incorrecto, solo permite 1 o 2");
         return 0;
     }
 
-    if (view < 1 || view > 2)
-    {
+    if (view < 1 || view > 2){
         printf("Visualizacion de los instantes incorrecto, solo permite 1 o 2");
         return 0;
     }
 
-    if (times < 0)
-    {
+    if (times < 0){
         printf("Numero de instantes incorrecto, solo permite numeros mayor o igual a 0");
         return 0;
     }
@@ -62,19 +57,24 @@ int main(int argc, char const *argv[])
     puts("\033[H\033[J");
     print_table(&game_of_life.table, "First Generation", game_of_life.generation);
     play(&game_of_life);
-    sleep(2);
+    
+    if (view == 2){
+    pause_game();
+    }else{
+        sleep(2);
+    }
     puts("\033[H\033[J");
-    do
-    {
+    do{
         puts("\033[H\033[J");
         print_table(&game_of_life.table, "Next Generation:", game_of_life.generation);
         play(&game_of_life);
-        if (view == 2)
-        {
+        if (view == 2){
             pause_game();
+        }else{
+            sleep(2);
         }
 
-        sleep(1);
+        
     } while (is_all_dead(&game_of_life.table) != 1 && is_all_moments(&game_of_life, times) != 0);
 
     return 0;
